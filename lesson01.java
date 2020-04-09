be586ace4c52a7b0c45116311b280f68a458e370
yamamoto <sky.shu4230@gmail.com>
2020-04-09 15:38:18 +0900

    Hello World

--- lesson01.java
+++ lesson01.java
@@ -1,5 +1,5 @@
-public class Main{
-     public satic void main(String[] args){
+public class lesson01{
+     public static void main(String[] args){
           System.out.println("Hello World");
      }
 }
