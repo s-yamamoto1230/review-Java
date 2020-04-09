3a8a11e9c4b7553f667f8a16a4534f7af2994f19
yamamoto <sky.shu4230@gmail.com>
2020-04-09 16:05:03 +0900

    Hello World

--- lesson01.java
+++ lesson01.java
@@ -1,5 +1,14 @@
-public class lesson01{
+public class lesson01{                       //class名とファイル名は同じにする。
      public static void main(String[] args){
           System.out.println("Hello World");
      }
 }
+/*
+public static void main(String[] args)の各項目の意味
+public    どこからでも参照可能
+static    インスタンス可(new)しなくても外部から使用可能
+void      戻り値なし
+main      メソッド名
+String[]  引数をString型の配列で受け取る
+args      引数名、argument（和訳：引数）の複数形でargumentsの略省
+*/
