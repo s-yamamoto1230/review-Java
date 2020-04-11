6d0f506d766314961fe9e7bbe2152ef9abeb6a56
yamamoto <sky.shu4230@gmail.com>
2020-04-11 15:24:33 +0900

    演算

---
+++ lesson03.java
@@ -0,0 +1,20 @@
+public class lesson03{
+     public static void main(String[] args){
+          int a = 20;
+          int b = a+5;   //足し算
+          int c = a-5;   //引き算
+          int d = a*5;   //掛け算
+          int e = a/5;   //割り算
+          int f = a%5;   //余りを求める(20/5=4余り0なので0が出てくる)
+          System.out.println(a);
+          System.out.println(b);
+          System.out.println(c);
+          System.out.println(d);
+          System.out.println(e);
+          System.out.println(f);
+          a++;      //aにプラス1する
+          b--;      //bをマイナス1する
+          System.out.println(a);
+          System.out.println(b);
+     }
+}
