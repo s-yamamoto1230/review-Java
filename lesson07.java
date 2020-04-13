92c5ad0bda187acb331824e0d3876e0d49dce5c6
yamamoto <sky.shu4230@gmail.com>
2020-04-13 14:48:53 +0900

    while文,do-while文

--- lesson07.java
+++ lesson07.java
@@ -1,11 +1,17 @@
 public class lesson07{
      public static void main(String[] args){
-          boolean kye = false;
-          while(kye == false){                         //kyeがfalseの間はループする
+          boolean kye = true;
+          //前判定  while(条件を見てからwhileに入る)
+          while(kye == false){                         //kyeがfalse間はループする
                System.out.println("鍵がかかってます");
-               System.out.println("今開けます");
-               kye = true;                             //kyeをtrueにする
+               System.out.println("今あけます");
           }
+          System.out.println("鍵が開きました");
+          //後判定  do-while(とりあえず一回回してからwhile条件を見る)
+          do{
+               System.out.println("鍵がかかってます");
+               System.out.println("今あけます");
+          }while(kye == false);
           System.out.println("鍵が開きました");
      }
 }
