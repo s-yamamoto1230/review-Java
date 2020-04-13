093154e7fbd410735ecbc603d3667c07b60767e2
yamamoto <sky.shu4230@gmail.com>
2020-04-13 13:22:33 +0900

    if文

---
+++ lesson06.java
@@ -0,0 +1,11 @@
+public class lesson06{
+     public static void main(String[] args){
+          boolean wether = true;
+          if(wether == true){
+               System.out.println("今日は晴れです");
+          }
+          else{
+          System.out.println("今日は雨です");
+          }
+     }
+}
